14a457c27067170bf55805953308252b1abd858f
Mike Caputo <therubikscubekid@gmail.com>
2021-01-03 12:56:14 -0600

    Add sink to ragdoll models

--- Assets/Zombies/Sink.cs
+++ Assets/Zombies/Sink.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class Sink : MonoBehaviour
 {
+    public float sinkDelay = 10;
     float destroyHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (this.gameObject.tag == "Ragdoll")
+            Invoke("StartSink", 5);
     }
 
     public void StartSink() {
         // get height of terrain at this's position (actually 5 meters under terrain)
         destroyHeight = Terrain.activeTerrain.SampleHeight(this.transform.position) - 5;
+
         Collider[] colliders = this.transform.GetComponentsInChildren<Collider>();
         foreach(Collider col in colliders)
         {
             Destroy(col);
         }
 
-        InvokeRepeating("SinkIntoGround", 10, 0.05f);
+        InvokeRepeating("SinkIntoGround", sinkDelay, 0.05f);
     }
 
     // Update is called once per frame
